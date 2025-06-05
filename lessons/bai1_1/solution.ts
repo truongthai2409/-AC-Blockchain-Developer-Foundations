7bbf1e526b905403e7067e082e8a2ece31cc4de9
Truong Thai <52000714@student.tdtu.edu.vn>
2025-06-05 17:26:00 +0700

    Lesson-1 State : Done

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+import { CLIENT_RENEG_LIMIT } from "tls";
 
 export type Block = {
   index: number;
@@ -10,5 +11,12 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const value = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const hash = crypto.createHash('sha256').update(value).digest('hex');
+  // console.log("hello")
+  
+  // console.log(hash);
+  // console.log(block.current_hash === hash);
+  return hash === block.current_hash;
+  // return false; // Chỉnh lại logic
 }
