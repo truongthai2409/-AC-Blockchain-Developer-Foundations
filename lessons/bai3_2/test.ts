daf7c96bde4ad4024885e9a2a2c6770db6d9a597
Trong Dinh <trongdth@gmail.com>
2025-06-09 09:48:40 +0700

    add bai3_2

--- lessons/bai3_2/test.ts
+++ lessons/bai3_2/test.ts
@@ -1,3 +1,3 @@
 // Bài này chạy trên Remix IDE
 // Không cần kiểm thử bằng TypeScript
-console.log("Test this contract manually in Remix IDE.");
+console.log("Test contract trực tiếp trong Remix.");
